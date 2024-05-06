22ecef1ad3e92b8d5e9a0c0367ab3e73ced89416
hougiken <kasama.satoshi@hougiken.com>
2024-05-06 21:22:16 +0900

    set test.html

--- silvis_carousel4ncas.js
+++ silvis_carousel4ncas.js
@@ -8,6 +8,33 @@
 // <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
 // <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
+// イニシャライズ
+$(document).ready(function(){
+    // メインプレゼンテーション告知を作る。
+	$("#carousel1").prepend(car_mp("小松 真美子","オフィス施工","5/8"));
+	$("#carousel1").append(car_mp("三輪 智士","フラワーバルーン","5/15"));
+	$("#carousel1").append(car_mp("松本 一矢","3Dアクセ","5/22"));
+
+    // 定例会動画カルーセルのURL等を設定
+    var car_video = document.getElementById("car_video");
+    car_video.addEventListener("click",function(event){
+        if( confirm("動画再生パスワード"+this.getAttribute("url")+"をクリップボードにコピーして動画を開く。" ){
+            navigator.clipboard.writeText(this.getAttribute("pass"));
+            window.open(this.getAttribute("url"));
+        }
+    });
+
+    // カルーセルを起動する。
+	$(".silvis_carousel").slick({
+        "dots":true,
+        "slidesToShow":3,
+        "slidesToScroll": 3,
+        "centerMode":true
+    });
+});
+
+
+
 // メインプレゼンテーションを告知するカルーセル(div Element)を返す。 
 function car_mp(name, category, date){
     var div = $("<div>",{class:"carousel_MainPresentation"});
