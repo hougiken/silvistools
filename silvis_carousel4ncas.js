2733dc446a7f2529c9fd13fe2f176c2c3cd311ba
hougiken <kasama.satoshi@hougiken.com>
2024-05-06 22:19:14 +0900

    update carousel

--- silvis_carousel4ncas.js
+++ silvis_carousel4ncas.js
@@ -12,13 +12,13 @@
 $(document).ready(function(){
     // メインプレゼンテーション告知を作る。
 	$("#carousel1").prepend(car_mp("小松 真美子","オフィス施工","5/8"));
-	$("#carousel1").append(car_mp("三輪 智士","フラワーバルーン","5/15"));
 	$("#carousel1").append(car_mp("松本 一矢","3Dアクセ","5/22"));
+	$("#carousel1").append(car_mp("三輪 智士","フラワーバルーン","5/15"));
 
     // 定例会動画カルーセルのURL等を設定
     var car_video = document.getElementById("car_video");
     car_video.addEventListener("click",function(event){
-        if( confirm("動画再生パスワード"+this.getAttribute("url")+"をクリップボードにコピーして動画を開く。" ){
+        if( confirm("動画再生パスワード"+this.getAttribute("pass")+"をクリップボードにコピーして動画を開く。" )){
             navigator.clipboard.writeText(this.getAttribute("pass"));
             window.open(this.getAttribute("url"));
         }
